ede91ee7d6567a28b7add30b365f1b4f434229b5
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-02-03 22:23:33 -0500

    header design

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,12 +2,33 @@ import React from "react";
 import { HeaderWrapper, 
         HeaderImage, 
         HeaderTitle, 
-        HeaderLogo } from "../header/headerStyled";
-        
+        HeaderLogo,
+        HeaderUserCoin,
+        ImageLogo,
+        ImageCoin,
+        ButtonCoin,
+        TextButtonCoin,
+        UserName } from "../header/headerStyled";
+import logo from '../../assets/aerolab-logo.svg'
+import coin from '../../assets/coin.svg'  
+
+const handleChange = () => {
+    alert("hola");
+  };
+
 export function Header() {
     return (
         <HeaderWrapper>
-            <HeaderLogo/>
+            <HeaderLogo>
+                 <ImageLogo src={logo} alt="Logo" />
+                <HeaderUserCoin>
+                    <UserName> Jenny Moreno Vallejo </UserName>
+                   <ButtonCoin>
+                   <TextButtonCoin>1000</TextButtonCoin>
+                     <ImageCoin src={coin} alt="Coin" onClick={() => handleChange()} />
+                   </ButtonCoin>   
+                </HeaderUserCoin>
+             </HeaderLogo>
             <HeaderImage>
                 <HeaderTitle> Electronics</HeaderTitle>
             </HeaderImage>
--- src/components/header/headerStyled.js
+++ src/components/header/headerStyled.js
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import header from '../../assets/header-x1.png'
-import logo from '../../assets/aerolab-logo.svg'
+ 
+
+const media = {
+    desktop: '@media(min-width: 768px)'
+}
 
 export const HeaderWrapper = styled.div`
-    width:100;
+    width:100%;
 `
 
 export const HeaderImage = styled.div`
-    width:100;
+    width:100%;
     display:flex;
     background-image: url(${header});
     height:30vw;
@@ -15,13 +19,94 @@ export const HeaderImage = styled.div`
 `
 
 export const HeaderLogo = styled.div`
-    background-image: url(${logo});
+    width:100%;
+    display:flex;
+    justify-content: space-between;
+    
+`
+
+export const ImageLogo = styled.img`
+  width: 25px;
+  height: 25px;
+  display: block;
+  padding: 1rem 0rem 1rem 2rem;
+   ${media.desktop} {
+        width: 36px;
+        height: 36px;
+    }
+`;
+
+export const HeaderUserCoin = styled.div`
+    width:60%;
+    display: flex;
+    flex-direction: row; 
+    padding: 1rem 0.5rem 1rem 2rem;
+    ${media.desktop} {
+        padding: 1rem 2rem 1rem 0rem;
+        width:20%;
+    }
+`
+
+export const ButtonCoin = styled.button`
+   height: 30px;
+   border: none;
+   display:flex;
+   justify-content: space-between;
+   padding:0.2rem;
+   border-radius: 1rem;
+   outline: none;
+   cursor: pointer;
+    ${media.desktop} {
+        height: 40px;
+    }
+`
+export const TextButtonCoin = styled.p`
+   font-family:'Source Sans Pro', sans-serif;
+   font-size:20px;
+   margin: 0 auto;
+   padding-left:0.5rem;
+   line-height: 1.314rem; 
+   ${media.desktop} {
+        font-size: 25px;
+        line-height: 2rem;
+    } 
 `
 
 export const HeaderTitle = styled.h6`
     font-family:'Source Sans Pro', sans-serif;
-    font-size:64px;
+    font-size:20px;
     color:#ffffff;
     align-self: center;
-    padding: 18rem 7rem 5rem   
+    padding-top: 1.5rem;
+    padding-left: 2rem;
+    margin:0px;
+    ${media.desktop} {
+        font-size:64px;
+        padding-top: 5rem;
+        padding-left: 4rem;
+    }
 `
+
+export const ImageCoin = styled.img`
+  width: 25px;
+  height: 25px; 
+   ${media.desktop} {
+        padding: 0.1rem;
+        width: 36px;
+        height: 36px;
+    }
+`;
+
+export const UserName = styled.h6`
+    height: auto;
+    font-family:'Source Sans Pro', sans-serif;
+    font-size:10px;
+    color:black;
+    align-self: center;
+    margin:0px;
+    padding: 0.5rem 0.5rem 0.5rem 0rem;
+    ${media.desktop} {
+        font-size:16px;
+    }
+`
+
