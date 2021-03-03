7f7e8fe34d4b32d3aa013ff7a7ade311248a8227
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-03-03 12:43:17 -0500

    filter component desktop

--- src/components/filter/filterStyled.js
+++ src/components/filter/filterStyled.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+
+const media = {
+  desktop: '@media(min-width: 768px)'
+}
+
 export const FilterWrapper = styled.div`
     width:100%;
     margin-top: 0px;
@@ -9,31 +14,37 @@ export const FilterBody = styled.div`
     display: flex;
     margin: 1rem;
     flex-direction:column;
-    /*padding-top: 30px;
-    padding-bottom: 30px;
-    padding-right: 15px;
-    margin: 0 10px;*/
+    ${media.desktop} {
+      flex-direction:row;
+    }
 `;
 
 export const FilterTittle = styled.h3`
-  //margin-left: 1rem;
-  //font-size: 24px;
   font-size:20px;
   color: #616161;
   text-align: center;
+  ${media.desktop} {
+    margin-left: 1rem;
+    font-size: 24px;
+    line-height: 24px;
+  }
 `;
 
 export const FilterVerticalLine = styled.span`
   margin: 0 0.3rem;
   border:thin solid #d9d9d9;
-  //margin: 0 1rem;
+  ${media.desktop} {
+    margin: 0 1rem;
+  } 
 `;
 
 export const FilterGray = styled.span`
   color: #a3a3a3;
   font-size:20px;
-  //font-size:24px;
-  /*margin-right: 1rem;*/
+  ${media.desktop} {
+    font-size:24px;
+    margin-right: 1rem;
+  }
 `;
 
 export const FilterButton = styled.button`
@@ -50,6 +61,10 @@ export const FilterButton = styled.button`
    color:#616161;
    text-align:center;
    padding: 1rem 0;
+   ${media.desktop} {
+      width:15%;
+      height:63px;
+    }
 `
 
 export const FilterLine = styled.hr`
